39c1c8f44148191d9bd360c7b81e06d1678963db
williamesanders05 <williamesanders05@gmail.com>
2025-06-12 09:42:26 -0500

    saving before i mess something up

--- init.lua
+++ init.lua
@@ -283,6 +283,13 @@ require('lazy').setup({
       },
     },
   },
+  {
+    'windwp/nvim-autopairs',
+    event = 'InsertEnter',
+    config = true,
+    -- use opts = {} for passing setup options
+    -- this is equivalent to setup({}) function
+  },
 
   -- NOTE: Plugins can also be configured to run Lua code when they are loaded.
   --
